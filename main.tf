374677522175c5468414fadc8625520c0d640635
williamqsnt <94827131+williamqsnt@users.noreply.github.com>
2024-04-02 10:46:49 +0000

    terraform

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
-provider 
+provider "aws" {
+  region = "votre_region_aws"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-1234567890abcdef0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
