fa4f67ecc10587568ebf485104007e6e1d53e026
williamqsnt <94827131+williamqsnt@users.noreply.github.com>
2024-04-02 10:52:06 +0000

    test

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key ID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key"
+}
+
+variable "region" {
+  description = "AWS Region"
+  default     = "eu-west-3"
+}
