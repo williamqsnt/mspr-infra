aa0bafac84e96b061f28f9ff9f71cdca407ab291
williamqsnt <94827131+williamqsnt@users.noreply.github.com>
2024-04-04 11:16:45 +0000

    installation docker

--- main.tf
+++ main.tf
@@ -19,6 +19,17 @@ resource "aws_instance" "mspr-ec2" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
 
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+              sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'
+              sudo apt-get update
+              sudo apt-get install -y docker-ce
+              sudo usermod -aG docker ${USER} # Ajoute l'utilisateur actuel au groupe docker
+              EOF
+
   tags = {
     Name = "mspr-ec2"
   }
