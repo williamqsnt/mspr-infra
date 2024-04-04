c511f7f8af0cf1c0d94403e073e66f40b0b0d801
williamqsnt <94827131+williamqsnt@users.noreply.github.com>
2024-04-04 11:19:01 +0000

    installation docker

--- main.tf
+++ main.tf
@@ -27,7 +27,6 @@ resource "aws_instance" "mspr-ec2" {
               sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'
               sudo apt-get update
               sudo apt-get install -y docker-ce
-              sudo usermod -aG docker ${USER} # Ajoute l'utilisateur actuel au groupe docker
               EOF
 
   tags = {
